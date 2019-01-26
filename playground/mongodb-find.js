97ff6f7852b68b962ab869eba991586fb6dac779
Amir Hajizadeh <amir@metart.com>
2019-01-25 16:46:18 -0800

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,41 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+const obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c4ba56141f2bab5fb4c4252')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Amir'}).toArray().then((docs) => {
+    console.log('Users:');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  db.collection('Users').find({name: 'Amir'}).count().then((count) => {
+    console.log(`User count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  // db.close();
+});
